e3f586de222f086c11720a510c88abae6238eb67
Marcos Fellipe Guimarães <marcosfgui2006@gmail.com>
2023-02-08 19:18:36 -0300

    Style:
    estilização do floatingbutton

--- lib/home.dart
+++ lib/home.dart
@@ -12,6 +12,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white ,
       appBar: AppBar(
         centerTitle: true,
         title: Text('Lista de Tarefas'),
@@ -23,6 +24,29 @@ class _HomeState extends State<Home> {
         child: Column(
           children: <Widget>[
 
+          ],
+        ),
+      ),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      /*floatingActionButton: FloatingActionButton(onPressed: (){print('Pressionado');}, backgroundColor: Colors.purple,
+                            elevation: 6,
+                            child: Icon(Icons.add),),
+
+       */
+      floatingActionButton: FloatingActionButton.extended(onPressed: (){print('Pressionado');}, backgroundColor: Colors.purple,
+        elevation: 6,
+        icon: Icon(Icons.add),
+        label: Text('Adicionar Tarefa'),
+        shape: BeveledRectangleBorder(
+          borderRadius: BorderRadius.circular(20)
+        ),),
+
+      bottomNavigationBar: BottomAppBar(
+        //shape: CircularNotchedRectangle(),
+        child: Row(
+          children: <Widget>[
+            IconButton(onPressed: (){}, icon: Icon(Icons.add))
           ],
         ),
       ),
