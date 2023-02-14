8b940e277342ee1de020d73e893f6727e1949ec4
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2023-02-14 15:47:05 -0300

    Dismissible

--- lib/dismissible.dart
+++ lib/dismissible.dart
@@ -9,6 +9,9 @@ class Dism extends StatefulWidget {
 }
 
 class _DismState extends State<Dism> {
+
+  List _lista = ["Jamilton","Maria","João","Carla"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold( 
@@ -22,7 +25,48 @@ class _DismState extends State<Dism> {
         padding: EdgeInsets.all(10),
         child: Column(
           children: <Widget>[
-            CheckboxListTile(value: null, onChanged: (bool? valor){}, )
+            Expanded(child: ListView.builder(
+                itemCount: _lista.length,
+                itemBuilder: (context, index){
+
+                  final item = _lista[index];
+
+                  return Dismissible(
+                      direction: DismissDirection.horizontal,
+                      background: Container(
+                        padding: EdgeInsets.all(16),
+                        color: Colors.red,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Icon(Icons.train, color: Colors.white,)
+                          ],
+                        ),
+                      ),
+                      secondaryBackground: Container(
+                        color: Colors.green,
+                        padding: EdgeInsets.all(16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            Icon(Icons.edit, color: Colors.white,)
+                          ],
+                        ),
+                      ),
+                      onDismissed: (direction){
+                        if(direction == DismissDirection.startToEnd){
+
+                        } else if(direction == DismissDirection.endToStart){
+
+                        }
+                      },
+                      key: Key(item),
+                      child: ListTile(
+                        title: Text(item),
+                      )
+                  );
+                }
+            )),
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,6 @@ import 'home.dart';
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Dism(),
+    home: Home(),
   ));
 }
